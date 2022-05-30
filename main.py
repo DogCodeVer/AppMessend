2dcc7a9cb9309de9334820bd6d9efed5a46c2a26
DogCodeVer <sheremetvadim09@gmail.com>
2022-05-30 23:45:55 +0300

    fix2

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from ShiftEncode import *
 
 class App():
     def sendMsg(self):
-
-        self.msg = Frame(self.bg_msg, bg='#272928')
-        self.msg.pack()
         msg_text = Label(self.msg,
                          text=self.ent_msg.get(),
                          bg='#383A39',
@@ -16,6 +13,7 @@ class App():
                          pady=0.1,
                          border='5')
         msg_text.pack()
+        msg_text.place()
 
 
         codded=encode(self.ent_msg.get(), self.s)
@@ -43,7 +41,8 @@ class App():
         self.s = []
 
         self.root = Tk()
-        self.root.geometry("1200x666")
+        self.root.title("Messenger")
+        self.root.geometry("360x610")
 
         self.bg_msg = Frame(self.root, bg='#272928')
         self.bg_msg.pack()
@@ -57,37 +56,44 @@ class App():
         self.btn_msg.pack()
         self.btn_msg.place(bordermode="ignore", width=55, height=35, x=290, y=560)
 
-        self.setting = Frame(self.root)
+        self.setting = Frame(self.bg_msg,bg='#272928')
         self.setting.pack()
-        self.setting.place(x=400, y=5)
+        self.setting.place(height=140, width=350)
 
-        self.txt_sum = Label(self.setting, text="Выбрать сумматоры")
-        self.txt_sum.grid(column=0, row=0)
+        self.txt_sum = Label(self.setting, text="Выбрать сумматоры",bg='#272928',fg='#E4E6E5')
+        self.txt_sum.pack(side='left')
 
         self.s1 = IntVar()
         self.s2 = IntVar()
         self.s3 = IntVar()
 
-        self.check1 = Checkbutton(self.setting, variable=self.s1, onvalue="1", offvalue="0")
-        self.check2 = Checkbutton(self.setting, variable=self.s2, onvalue="2", offvalue="0")
-        self.check3 = Checkbutton(self.setting, variable=self.s3, onvalue="3", offvalue="0")
+        self.check1 = Checkbutton(self.setting, variable=self.s1, onvalue="1", offvalue="0",bg='#272928')
+        self.check2 = Checkbutton(self.setting, variable=self.s2, onvalue="2", offvalue="0",bg='#272928')
+        self.check3 = Checkbutton(self.setting, variable=self.s3, onvalue="3", offvalue="0",bg='#272928')
 
-        self.check1.grid(column=1, row=0)
-        self.check2.grid(column=2, row=0)
-        self.check3.grid(column=3, row=0)
+        self.check1.pack(side="left")
+        self.check2.pack(side="left")
+        self.check3.pack(side="left")
 
         self.btn_sum = Button(self.setting, text="Добавить", command=lambda: (self.addSum(self.s1, self.s2, self.s3)))
-        self.btn_sum.grid(row=1)
+        self.btn_sum.pack(side='left')
 
-        self.summators = Label(self.setting, text="Сумматоры:{}".format(self.s))
-        self.summators.grid(row=2)
+        self.summators = Label(self.setting, text="Сумматоры:{}".format(self.s),bg='#272928',fg='#E4E6E5')
+        self.summators.pack()
+        self.summators.place(x=0,y=0)
 
-        self.encoded=Label(self.setting,text="Закодированое сообщение:")
-        self.encoded.grid(row=3)
+        self.encoded=Label(self.setting,text="Закодированое сообщение:",bg='#272928',fg='#E4E6E5')
+        self.encoded.pack()
+        self.encoded.place(y=20)
 
-        self.decoded=Label(self.setting,text="Декодированое сообщение:")
-        self.decoded.grid(row=4)
 
+        self.decoded=Label(self.setting,text="Декодированое сообщение:",bg='#272928',fg='#E4E6E5')
+        self.decoded.pack()
+        self.decoded.place(y=40)
+
+        self.msg = Frame(self.bg_msg, bg='#272928')
+        self.msg.pack()
+        self.msg.place(x=5,y=100)
 
 app = App()
 app.root.mainloop()
